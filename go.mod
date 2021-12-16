f2dc1fe52cd5e1c513b0c3eb2e4292e398af81bd
s9256001 <s9256001@cs.nchu.edu.tw>
2021-12-16 14:31:49 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module decimal_wrap1
+
+go 1.15
+
+require github.com/shopspring/decimal v1.3.1
---
+++ main.go
@@ -0,0 +1,12 @@
+package decimalwrap1
+
+import (
+	"fmt"
+
+	"github.com/shopspring/decimal"
+)
+
+func TFunc() {
+	v, _ := decimal.NewFromString("-123.4567")
+	fmt.Printf("v1.3.1: %s\n", v)
+}
